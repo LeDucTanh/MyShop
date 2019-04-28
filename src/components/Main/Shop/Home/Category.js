29ade3c4d1da70d563fb5eea10ed6e6be5f0b752
Tanh Le D <tanh13t2@gmail.com>
2019-04-28 16:10:25 +0700

    Use Swiper for creating category

--- src/components/Main/Shop/Home/Category.js
+++ src/components/Main/Shop/Home/Category.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
-import bannerImage from '../../../../media/temp/banner.jpg';
+import Swiper from 'react-native-swiper';
+import littleIcon from '../../../../media/temp/little.jpg';
+import maxiIcon from '../../../../media/temp/maxi.jpg';
+import partyIcon from '../../../../media/temp/party.jpg';
 
 const { width, height } = Dimensions.get('window');
 
 export default class Category extends Component {
     render() {
-        const { wrapper, textStyle, imageStyle } = styles;
+        const { wrapper, textStyle, imageStyle, itemName } = styles;
         return (
             <View style={wrapper}>
                 <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <Text style={textStyle}>SPRING COLLECTION</Text>
+                    <Text style={textStyle}>LIST OF CATEGORY</Text>
                 </View>
                 <View style={{ flex: 4 }}>
-                    <Image source={bannerImage} style={imageStyle} />
+                    <Swiper >
+                        <View>
+                            <Image source={littleIcon} style={imageStyle} />
+                            <Text style={itemName}>Little Dress</Text>
+                        </View>
+                        <View>
+                            <Image source={maxiIcon} style={imageStyle} />
+                            <Text style={itemName}>Maxi Dress</Text>
+                        </View>
+                        <View>
+                            <Image source={partyIcon} style={imageStyle} />
+                            <Text style={itemName}>Party Dress</Text>
+                        </View>
+                    </Swiper>
                 </View>
             </View>
         );
@@ -41,5 +57,16 @@ const styles = StyleSheet.create({
     imageStyle: {
         height: imageHeight,
         width: imageWidth
+    },
+    itemName: {
+        position: 'absolute',
+        bottom: 0,
+        top: imageHeight / 2,
+        left: (width / 2) - 65,
+        right: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'Avenir',
+        fontSize: 20,
     }
 });
